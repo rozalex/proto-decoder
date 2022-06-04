92d33940a4e988853931d7f999b70c1bf534bf08
rozalex <rozalex@gmail.com>
2022-06-04 10:37:36 +0300

    add alert messages

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ import TextareaAutosize from '@mui/material/TextareaAutosize';
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
 import Autocomplete from '@mui/material/Autocomplete';
+import Stack from '@mui/material/Stack';
+import Snackbar from '@mui/material/Snackbar';
+import MuiAlert, { AlertProps } from '@mui/material/Alert';
 import {priority} from '@prioritysoftware/priority-proto-api/lib/nform';
 
 const filterOptions = [];
@@ -15,17 +18,34 @@ for (let key in form) {
   filterOptions.push({"label": key, "decode": form[key].decode})
 }
 
+const Alert = React.forwardRef(function Alert(props, ref) {
+  return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+});
+
 function App() {
   const [encodedText, setEncodedText] = useState("");
   const [decoder, setDecoder] = useState("");
+  const [alertState, setAlertState] = useState(false);
+  const [alertText, setAlertText] = useState(false);
+  const [alertType, setAlertType] = useState(false);
 
   const decodeText = () => {
-    let buf64 = util.newBuffer(util.base64.length(encodedText));
-    util.base64.decode(encodedText, buf64, 0);
-    const text = decoder.decode(buf64);
-    console.log(text);
-  }
+    try {
+      let buf64 = util.newBuffer(util.base64.length(encodedText));
+      util.base64.decode(encodedText, buf64, 0);
+      const text = decoder.decode(buf64);
+      console.log(text);
 
+      setAlertType("success");
+      setAlertText("Decoded successfully, check console for result");
+    } catch(err) {
+      setAlertType("error");
+      setAlertText("Error decoding, make sure you have selected a correct decoder");
+    }
+
+    setAlertState(true);
+  }
+    
   return (
     <div className="App">
         <Autocomplete
@@ -33,7 +53,7 @@ function App() {
           onChange={(event, value) => {setDecoder(value)}}
           options={filterOptions}
           sx={{ width: 300 }}
-          renderInput={(params) => <TextField {...params} label="Service" />}
+          renderInput={(params) => <TextField {...params} label="Decoder" />}
         />
         <TextareaAutosize
           onChange={ev => setEncodedText(ev.target.value)}
@@ -41,6 +61,11 @@ function App() {
           style={{ width: 600, height: 300, marginTop: 5 }}
         />
       <Button width={"100px"} variant="contained" onClick={decodeText}>Decode</Button>
+      <div className="alert">
+        <Snackbar open={alertState} autoHideDuration={4000} onClose={() => {setAlertState(false)}}>
+          <Alert severity={alertType}>{alertText}</Alert>
+        </Snackbar>
+      </div>
     </div>
   );
 }

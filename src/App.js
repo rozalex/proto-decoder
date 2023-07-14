32171e76c78ccce942a30fff4cfd8f74ee699b3e
Alex Rozhavsky <alex.rozhavsky@priority-software.com>
2023-07-14 09:32:46 +0300

    modifications

--- src/App.js
+++ src/App.js
@@ -1,45 +1,96 @@
 import React, { useState } from 'react';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 import './App.css';
-
-import { util } from 'protobufjs';
 import TextareaAutosize from '@mui/material/TextareaAutosize';
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
 import Autocomplete from '@mui/material/Autocomplete';
 import Snackbar from '@mui/material/Snackbar';
 import MuiAlert from '@mui/material/Alert';
-import {priority} from '@prioritysoftware/priority-proto-api/lib/nform';
+import { util } from 'protobufjs';
 import { JSONTree } from 'react-json-tree';
+import { priority } from '@prioritysoftware/priority-proto-api/lib/nform';
 
-const filterOptions = [];
 const form = priority.netitems.form;
+const ngtw = priority.netitems.ngtw;
 
-for (let key in form) {
-  filterOptions.push({"label": key, "decode": form[key].decode})
-}
+const filterOptions = Object.entries({ ...form, ...ngtw }).map(([key, value]) => ({
+  label: key,
+  decode: value.decode
+}));
 
 const Alert = React.forwardRef(function Alert(props, ref) {
   return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+});
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+  components: {
+    MuiInputBase: {
+      styleOverrides: {
+        root: {
+          color: '#fff',
+        },
+      },
+    },
+    MuiOutlinedInput: {
+      styleOverrides: {
+        root: {
+          color: '#fff',
+          borderColor: '#999',
+        },
+      },
+    },
+    MuiFormLabel: {
+      styleOverrides: {
+        root: {
+          color: '#fff',
+        },
+      },
+    },
+    MuiAutocomplete: {
+      styleOverrides: {
+        paper: {
+          backgroundColor: '#333',
+        },
+      },
+    },
+    MuiButton: {
+      styleOverrides: {
+        contained: {
+          backgroundColor: '#666',
+          color: '#fff',
+          '&:hover': {
+            backgroundColor: '#888',
+          },
+        },
+      },
+    },
+    MuiAlert: {
+      styleOverrides: {
+        root: {
+          backgroundColor: '#333',
+          color: '#fff',
+        },
+      },
+    },
+  },
 });
 
 function App() {
   const [encodedText, setEncodedText] = useState("");
   const [decoder, setDecoder] = useState("");
   const [alertState, setAlertState] = useState(false);
-  const [alertText, setAlertText] = useState(false);
-  const [alertType, setAlertType] = useState(false);
+  const [alertText, setAlertText] = useState("");
+  const [alertType, setAlertType] = useState("");
   const [json, setJSON] = useState();
-
-  const selectText = (containerid) => {
-    const range = document.createRange();
-    range.selectNode(document.getElementById(containerid));
-    window.getSelection().removeAllRanges();
-    window.getSelection().addRange(range);
-  }
+  const [isExpanded, setIsExpanded] = useState(false); // Track expansion state
 
   const decodeText = () => {
     try {
-      let buf64 = util.newBuffer(util.base64.length(encodedText));
+      const buf64 = util.newBuffer(util.base64.length(encodedText));
       util.base64.decode(encodedText, buf64, 0);
       const text = decoder.decode(buf64);
       console.log(text);
@@ -47,45 +98,82 @@ function App() {
       setJSON(text);
       setAlertType("success");
       setAlertText("Decoded successfully");
-    } catch(err) {
+    } catch (err) {
       setAlertType("error");
-      setAlertText("Error decoding, make sure you have selected a correct decoder");
+      setAlertText("Error decoding, make sure you have selected the correct decoder");
     }
 
     setAlertState(true);
-  }
-    
-  return (
-    <div className="App">
-        <Autocomplete
-          disablePortal
-          onChange={(event, value) => {setDecoder(value)}}
-          options={filterOptions}
-          sx={{ width: "80vw" }}
-          renderInput={(params) => <TextField {...params} label="Decoder" />}
-        />
-        <div className="wrap">
-          <TextareaAutosize
-            onChange={ev => setEncodedText(ev.target.value)}
-            value={encodedText}
-            style={{ width: "40vw", height: "50vh"}}
-          />
-          <div className="result">
-            <pre id="preelement">{JSON.stringify(json, null, 2) }</pre>
-          </div>
-        </div>
-        <Button width={"10vw"} variant="contained" onClick={decodeText}>Decode</Button>
+  };
 
-        <div className="tree">
-          {json && <JSONTree data={json} />}
-        </div>
-        <div className="alert">
-          <Snackbar open={alertState} autoHideDuration={1000} onClose={() => {setAlertState(false)}}>
-            <Alert severity={alertType}>{alertText}</Alert>
-          </Snackbar>
-        </div>
-    </div>
-  )
+  const toggleExpansion = () => {
+    setIsExpanded(!isExpanded);
+  };
+
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <div className={`App ${isExpanded ? "expanded" : ""}`}>
+        <main className="App-content">
+          {!isExpanded && (
+            <div className="input-section">
+              <div className="input-group">
+                <Autocomplete
+                  disablePortal
+                  onChange={(event, value) => { setDecoder(value) }}
+                  options={filterOptions}
+                  sx={{ width: "50%" }} // Set the width of the Autocomplete component
+                  renderInput={(params) => (
+                    <TextField
+                      {...params}
+                      label="Decoder"
+                      variant="outlined"
+                    />
+                  )}
+                />
+                <Button
+                  variant="contained"
+                  onClick={decodeText}
+                  disabled={!decoder}
+                >
+                  Decode
+                </Button>
+              </div>
+
+              <TextareaAutosize
+                className="textarea"
+                onChange={ev => setEncodedText(ev.target.value)}
+                value={encodedText}
+                placeholder="Enter encoded text..."
+              />
+            </div>
+          )}
+
+          <div className={`result-section ${isExpanded ? "expanded" : ""}`}>
+            <div className={`result ${isExpanded ? "expanded" : ""}`}>
+              <Button onClick={toggleExpansion}>
+                {isExpanded ? 'Collapse' : 'Expand'}
+              </Button>
+              <pre id="preelement" >
+                {JSON.stringify(json, null, 2)}
+              </pre>
+            </div>
+
+            <div className="tree">
+              {json && <JSONTree data={json} />}
+            </div>
+
+            <Snackbar
+              open={alertState}
+              autoHideDuration={3000}
+              onClose={() => { setAlertState(false) }}
+            >
+              <Alert severity={alertType}>{alertText}</Alert>
+            </Snackbar>
+          </div>
+        </main>
+      </div>
+    </ThemeProvider>
+  );
 }
 
 export default App;
